a893d2c9696c2b84dead66f2297341d81fa6cce7
trimikpro <vupotiroy112@gmail.com>
2025-01-10 15:44:04 +0600

    cart collections

--- index.js
+++ index.js
@@ -26,19 +26,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("carts");
 
-    app.get('/menu', async( req, res) => {
-        const result = await menuCollection.find().toArray();
-        res.send(result); 
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
     })
-    app.get('/reviews', async( req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result); 
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
     })
 
-
+    // cartCollections 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
 
 
 
@@ -55,8 +68,26 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('boss is sitting')
+  res.send('boss is sitting')
 })
-app.listen(port, ()=> {
-    console.log(`bistro boss sitting on port ${port}`)
+app.listen(port, () => {
+  console.log(`bistro boss sitting on port ${port}`)
 })
+
+
+
+/**
+ * -----------------------------------------
+ *         NAMING CONVENTION 
+ * -----------------------------------------
+ * 
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('/users')
+ * app.put('/users/:id')
+ * app.patch('/users/:id')
+ * app.delete('/users/:id')
+ * 
+ * 
+ *     
+ */
